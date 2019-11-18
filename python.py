8ba4a6e9c31cbcd585b79c2b555bbb2ed46a0d38
valuoch <val.valentine227@gmail.com>
2019-11-18 20:29:43 +0300

    MyFirstPythonClass

---
+++ python.py
@@ -0,0 +1,27 @@
+print("Hello World")
+
+
+my_name = "Valentine"
+print(my_name)
+age = 26
+print(type(age))
+gender = "F"
+marriage = True
+print(type(marriage))
+my_name = "Aluoch"
+print(my_name)
+print(age)
+print(gender)
+print(marriage)
+
+z = 'I love programming'
+new_var=z[-1]
+new_var=z[9:16]
+print(new_var)
+
+y = 'My python class'
+new_var=y[-5:-1]
+print(new_var)
+
+my_friend = "halima"
+print(my_friend.capitalize())
