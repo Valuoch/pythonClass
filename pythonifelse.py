1e124d55afac33d776d2b03160bbc84ca18fac29
valuoch <val.valentine227@gmail.com>
2019-11-21 15:57:51 +0300

    pythonbasics

--- pythonifelse.py
+++ pythonifelse.py
@@ -31,20 +31,68 @@
 #     print(" {} is a negative number".format(num))
 
 # num = -100
-num = int(input("Enter a number:"))
-if num > 0:
-    print(" {} is > 0".format(num))
-
-elif num == 10:
-    print(" {} is == 0".format(num))
-
-else:
-    print(" {} is a negative number".format(num))
+# num = int(input("Enter a number:"))
+# if num > 0:
+#     print(" {} is > 0".format(num))
+#
+# elif num == 10:
+#     print(" {} is == 0".format(num))
+#
+# else:
+#     print(" {} is a negative number".format(num))
 
 
 # Assigment:
-#1. Create a program to ask student name and ask for marks of 5 subjects eg kiswahili, english etc
+#1.
+#  of 5 subjects eg kiswahili, english etc
 #     a)find total marks
-#     b)find the average makes
+#     b)find the average marks
 #     c)grade the student
+
+#     a)find total marks
+
+# kis = int(input("Enter marks for Kiswahili:"))
+# eng = int(input("Enter marks for English:"))
+# maths = int(input("Enter marks for Maths:"))
+# chem = int(input("Enter marks for Chemestry:"))
+# bio= int(input("Enter marks for Biology:"))
+
+# sum= int(kis) + int(eng) + int(maths) + int(chem) + int(bio)
+# print(sum)
+
+
+#     b)find the average marks
+
+# kis = int(input("Enter marks for Kiswahili:"))
+# eng = int(input("Enter marks for English:"))
+# maths = int(input("Enter marks for Maths:"))
+# chem = int(input("Enter marks for Chemestry:"))
+# bio= int(input("Enter marks for Biology:"))
+#
+# average = (kis+eng+maths+chem+bio)/5
+# print(average)
+
+# c)grade the student
+
+# kis = int(input("Enter marks for Kiswahili:"))
+# eng = int(input("Enter marks for English:"))
+# maths = int(input("Enter marks for Maths:"))
+# chem = int(input("Enter marks for Chemestry:"))
+# bio= int(input("Enter marks for Biology:"))
+#
+# average = (kis+eng+maths+chem+bio)/5
+
+# if (average>=90):
+#     print("Grade: A")
+# elif(average>=80,average<90):
+#     print("Grade: B")
+# elif(average>=70,average<80):
+#     print("Grade: C")
+# elif(average>=60, average<70):
+#     print("Grade: D")
+
+# else:
+#     print("Grade E")
+
+
 #2.assume u have makes for 10 students and rank them
